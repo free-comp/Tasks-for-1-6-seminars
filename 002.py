a68ed03adb5fc4feda7546010e84b764b21cbc36
AlisBrk <free-comp@yandex.ru>
2022-02-07 12:21:13 +0300

    Добавлен поиск max для любого количества чисел

--- 002.py
+++ 002.py
@@ -19,3 +19,16 @@ maximum = max (max_3, e)
 
 print(f'Maximum = {maximum}')
 
+## для любого количества чисел (через список)
+# def max (array):
+#     max = array[0]
+#     for i in array:
+#         if i > max:
+#             max = i
+#     return max
+
+# list_1 = [11, 6, -6, -44, 647, 23, 87]
+# list_1 = [int(i) for i in input('Введите значения через пробел ').split()]
+# result = max(list_1)
+# print ('Maximum = ', result)
+
