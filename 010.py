0cfcb3324f8b0dddb334c413c622f06a725f7aa7
AlisBrk <free-comp@yandex.ru>
2022-02-08 12:43:26 +0300

    Исправлена опечатка в задаче 10

--- 010.py
+++ 010.py
@@ -7,12 +7,12 @@ def distance (x1, y1, z1, x2, y2, z2):
     return dist
 
 print ('Задайте координаты для точки в пространстве: ')
-x1 = int (input(f'x1: '))
-y1 = int (input(f'y1: '))
-z1 = int (input(f'z1: '))
-x2 = int (input(f'x2: '))
-y2 = int (input(f'y2: '))
-z2 = int (input(f'z2: '))
+x1 = int (input('x1: '))
+y1 = int (input('y1: '))
+z1 = int (input('z1: '))
+x2 = int (input('x2: '))
+y2 = int (input('y2: '))
+z2 = int (input('z2: '))
 
 result = distance (x1, y1, z1, x2, y2, z2)
 print ('Расстоние между двумя точками в пространстве равно', round(result, 3))
